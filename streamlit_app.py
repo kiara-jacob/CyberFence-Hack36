8e0287aec58acee49c60de9d467b2b7a9ce72ce4
Kiara Jacob <kiarajacob03@gmail.com>
2022-04-23 22:53:47 +0530

    streamlit page update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from streamlit_option_menu import option_menu
 
 
 header = st.container()
@@ -7,18 +8,46 @@ dataset = st.container()
 features= st.container()
 modelTraining = st. container()
 
-with header:
-    st.title("insert name")
-    st.text("add description")
+with st.sidebar:
+    selected = option_menu(
+        menu_title=None,
+        options=['Cyber Bullying Detector','Harassment Detector'],
+    )
 
-with dataset:
-    st.header("Our dataset")
-    data = pd.read_csv("data/bully1.csv")
-    st.write(data.head())
+if selected == "Cyber Bullying Detector":
+    st.text(f"You have selected bullying model") #replace with the model page
+    with header:
+        st.title("CyberFence")
+        st.text("add description of bullying model")
 
-with features:
-    st.header("Features")
+    with dataset:
+        st.header("Our dataset")
+        data = pd.read_csv("data/data_final.csv")
+        st.write(data.head())
 
-with modelTraining:
-    st.header("The model")
+    with features:
+        st.header("Features")
+
+    with modelTraining:
+        st.header("The model")
+
+if selected == "Harassment Detector":
+    st.text(f"You have selected harassment model") #replace with the model page
+    with header:
+        st.title("CyberFence")
+        st.text("add description of harassment model")
+
+    with dataset:
+        st.header("Our dataset")
+        data = pd.read_csv("data/data_final_harassment.csv")
+        st.write(data.head())
+
+    with features:
+        st.header("Features")
+
+    with modelTraining:
+        st.header("The model")
+
+
+
 
